896a1640affc30bbeedada587b20a6f760e3af24
PeachPaulison <84045459+PeachPaulison@users.noreply.github.com>
2021-09-09 22:04:55 -0400

    Fixed formatting and changed Fetch

--- script.js
+++ script.js
@@ -2,42 +2,48 @@
 
 //const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
 
-window.addEventListener("load", function() {
+window.addEventListener('load', function() {
+	let listedPlanets;
+	// Set listedPlanetsResponse equal to the value returned by calling myFetch()
+	let listedPlanetsResponse = myFetch();
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-  let listedPlanetsResponse = myFetch();
+	listedPlanetsResponse
+		.then(function(result) {
+			listedPlanets = result;
+			console.log(listedPlanets);
+		})
+		.then(function() {
+			console.log(listedPlanets);
+			// Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+			const planet = pickPlanet(listedPlanets);
+			addDestinationInfo(
+				document,
+				planet.name,
+				planet.diameter,
+				planet.star,
+				planet.distance,
+				planet.moons,
+				planet.image
+			);
+		});
 
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-      const planet = pickPlanet(listedPlanets)
-      addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image)
-   })
+	let form = document.querySelector('form');
 
-   let form = document.querySelector("form");
+	//    get the form
+	//    add a listener to when the form submit
+	form.addEventListener('submit', function(event) {
+		event.preventDefault();
+		let pilot = document.querySelector('input[name=pilotName]');
+		let copilot = document.querySelector('input[name=copilotName]');
+		let fuelLevel = document.querySelector('input[name=fuelLevel]');
+		let cargoLevel = document.querySelector('input[name=cargoLevel]');
+		//let pilotInput = document.querySelector("input[name=pilotName]");
+		const pilotValue = pilot.value;
+		const copilotValue = copilot.value;
+		const fuelLevelValue = fuelLevel.value;
+		const cargoLevelValue = cargoLevel.value;
 
-//    get the form
-//    add a listener to when the form submit
-   form.addEventListener('submit', function(event) {
-     event.preventDefault();
-     let pilot = document.querySelector("input[name=pilotName]");
-     let copilot = document.querySelector("input[name=copilotName]");
-     let fuelLevel = document.querySelector("input[name=fuelLevel]");
-     let cargoMass = document.querySelector("input[name=cargoMass]");
-     let pilotInput = document.querySelector("input[name=pilotName]");
-     const pilotValue = pilot.value;
-     const copilotValue = copilot.value;
-     const fuelLevelValue = fuelLevel.value;
-     const cargoMassValue = cargoMass.value;
-
-
-      let list = document.getElementById('faultyItems');
-      formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoLevelValue)
-
-
-});
+		let list = document.getElementById('faultyItems');
+		formSubmission(document, list, pilotValue, copilotValue, fuelLevelValue, cargoLevelValue);
+	});
 });
